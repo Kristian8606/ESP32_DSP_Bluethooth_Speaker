96cc4ac5aa0786f320372e52a1ec840f9b86edf4
Kristian Dimitrov <kriso8606@gmail.com>
2024-07-13 17:52:25 +0300

    Add files via upload

---
+++ main/app_priv.h
@@ -0,0 +1,19 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#pragma once
+
+//#include "dsps_biquad_gen.h"
+//#include "dsps_biquad_gen_f32.c"
+#include "dsp.h"
+
+static void set_sample_rate (uint8_t samplerate);
+
+
+
+static void runFilters();
